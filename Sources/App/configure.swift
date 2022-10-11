9b5adcea1239d92d388575988f8589cf688409d6
AntonioIacono <antonio.iac98@outlook.it>
2022-10-11 22:38:10 +0200

    Updated config for database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,13 +8,24 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "Localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    if let urlString = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: urlString){
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgresConfig.tlsConfiguration = tlsConfig
+        
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    }else{
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "Localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+    }
+    
+  
 
     app.migrations.add(CreateIngredient())
     app.migrations.add(CreateRecipe())
@@ -24,9 +35,9 @@ public func configure(_ app: Application) throws {
     app.migrations.add(CreateBill())
     
 //    app.migrations.add(CreateOrdination())
+    if app.environment == .development{
     try app.autoMigrate().wait()
-    app.views.use(.leaf)
-
+    }
     
 
     // register routes
