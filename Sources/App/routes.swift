c829db17ad3eb29c3669686de5c29b9400ade803
AntonioIacono <antonio.iac98@outlook.it>
2022-10-12 00:44:04 +0200

    Updated config for migrations4

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,11 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+
+    app.get{ req in
+        return "it Works!"
+    }
     try app.register(collection: DrinkController())
     try app.register(collection: IngredientController())
+    
 }
